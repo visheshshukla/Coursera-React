ce24e67fa490d8d527d9067b65a816c59602929c
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-18 16:52:47 +0530

    single page applications part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,6 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -35,12 +34,21 @@ class Main extends Component {
             />
         );
       }
+
+      const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
+
         return (
             <div>
                 <Header/>
                 <Switch>
                   <Route path="/home" component={HomePage}/>
                   <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                  <Route path='/menu/:dishId' component={DishWithId} />
                   <Route exact path="/contactus" component={Contact} />
                   <Redirect to="/home"/>
                 </Switch>
