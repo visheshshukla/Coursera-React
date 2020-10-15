7817f889a49a19a9450c3c4e7366f13e737ee360
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-16 01:21:27 +0530

    bug fixes

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle,  Breadcrumb, BreadcrumbItem,  Button, Modal, ModalHeader, ModalBody, Label, Input, Form, FormGroup, Col,  Row } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle,  Breadcrumb, BreadcrumbItem,  Button, Modal, ModalHeader, ModalBody, Label, Input, Col,  Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
